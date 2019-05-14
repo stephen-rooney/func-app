2cd7282c94859d41da43d438b23db4ab77060881
Stephen Rooney <stephen.rooney@brightlittleideas.com>
2019-05-14 10:41:05 -0400

    update to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './about/about.module#AboutPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'calendar-help', loadChildren: './calendar/calendar-help/calendar-help.module#CalendarHelpPageModule' },
-  { path: 'about', loadChildren: './about/about.module#AboutPageModule' }
 ];
 @NgModule({
   imports: [
